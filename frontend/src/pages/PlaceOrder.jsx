5fbee73ef3bc9b3908729867101110ae2dab5d7c
vitoralexx <vitoralexse@gmail.com>
2025-02-28 11:22:33 -0300

    Add Razorpay payment method

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -28,6 +28,35 @@ const PlaceOrder = () => {
     setFormData(data => ({...data, [name]:value}))
   }
 
+  const initPay = (order) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Order Payment',
+      description: 'Order Payment',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
+        console.log(response)
+        try {
+
+          const { data } = await axios.post(backendUrl + '/api/order/verifyRazorpay', response, {headers: {token}})
+          if(data.success) {
+            navigate('/orders')
+            setCartItems({})
+          }
+
+        } catch (error) {
+            console.log(error)
+            toast.error(error)
+        }
+      }
+    }
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+  }
+
   const onSubmitHandler = async (event) => {
     event.preventDefault()
 
@@ -74,15 +103,19 @@ const PlaceOrder = () => {
           } else {
             toast.error(responseStripe.data.message)
           }
-
           break;
 
+        case 'razorpay':
+          const responseRazorpay = await axios.post(backendUrl + '/api/order/razorpay', orderData, {headers: {token}})
+          if(responseRazorpay.data.success) {
+            initPay(responseRazorpay.data.order)
+          } else {
+            toast.error(responseStripe.data.message)
+          }
+          break;
 
         default:
           break;
-
-
-
       }
 
     } catch (error) {
